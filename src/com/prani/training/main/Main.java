3c5080a346ae00f2ffe6cb2513c7fa540b7f4671
praneetha <gpraneetha924@gmail.com>
2025-05-15 23:57:52 +0530

    Added values don't comment it

--- src/com/prani/training/main/Main.java
+++ src/com/prani/training/main/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Customer.map.put("cust_123", customer);
         System.out.println(Customer.checkCustomerExistency("cust_123"));
         System.out.println(customer.checkCustomer("cust"));
-        int a =10;
-        System.out.println(a);
+        //added comment
+        System.out.println(10+20);
     }
 }
